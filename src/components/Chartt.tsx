5dbd13131779cd48cebf67587c3f13ae74659d32
Latemarch <dolphin0331@naver.com>
2023-06-19 22:56:14 +0900

    add chart

---
+++ src/components/Chartt.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Chart from "react-apexcharts";
+
+export default function Chartt() {
+	return (
+		<div>
+			<Chart
+				options={{
+					chart: {
+						id: "basic-bar",
+					},
+					xaxis: {
+						categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+					},
+				}}
+				series={[
+					{
+						name: "series-1",
+						data: [30, 40, 45, 50, 49, 60, 70, 91],
+					},
+				]}
+				type="bar"
+				width="500"
+			/>
+		</div>
+	);
+}
