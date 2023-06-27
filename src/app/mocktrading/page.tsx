9bde95da74c69fba13eff7722ee435a1acf4ed8f
Latemarch <dolphin0331@naver.com>
2023-06-28 00:41:45 +0900

    add fetching function with exception

--- src/app/mocktrading/page.tsx
+++ src/app/mocktrading/page.tsx
@@ -3,7 +3,7 @@ import HistoryPannel from "./compoenents/HistoryPannel";
 import { getRandomInt } from "@/libs/client/utils";
 import ButtonContainer from "./compoenents/ButtonContainer";
 import MyWallet from "./compoenents/MyWallet";
-import NavBar from "@/components/NavBar";
+import { getCandles } from "@/libs/server/fetchingFtns";
 const ChartContainer = dynamic(() => import("./compoenents/ChartContainer"), {
 	ssr: false,
 });
@@ -13,10 +13,11 @@ const url = process.env.NEXT_PUBLIC_API_URL!!;
 export default async function mocktraiding() {
 	const openTime = getRandomInt(100, 1239);
 	const fileNumber = getRandomInt(1, 64);
-	const candles = await fetch(`${url}/api/data/${fileNumber}`)
-		.then((res) => res.json())
-		.then((data) => data.data);
+	// const candles = await fetch(`${url}/api/data/${fileNumber}`)
+	// 	.then((res) => res.json())
+	// 	.then((data) => data.data);
 	// const candles = (await getCandle()) as candles;
+	const candles = await getCandles(`${url}/api/data/${fileNumber}`);
 
 	return (
 		<section className="h-full p-4 md:flex md:gap-3">
--- src/libs/server/fetchingFtns.ts
+++ src/libs/server/fetchingFtns.ts
@@ -1,3 +1,5 @@
+import { getCandle } from "../client/utils";
+
 export const getUserBalance = async () => {
 	const res = await fetch("http://localhost:3000/api/user/balance");
 	if (!res.ok) {
@@ -7,4 +9,17 @@ export const getUserBalance = async () => {
 	const data = await res.json();
 	console.log(data);
 	return data.data.balance;
+};
+
+export const getCandles = async (url: string) => {
+	let candles: any;
+	try {
+		candles = await fetch(url)
+			.then((res) => res.json())
+			.then((data) => data.data);
+	} catch (e) {
+		console.error("Error fetching data(candles):", e);
+		candles = [];
+	}
+	return candles;
 };
