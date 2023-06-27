21412b1da7915599927eaa85d51118b607dc0cb8
Latemarch <dolphin0331@naver.com>
2023-06-28 00:22:46 +0900

    fix error

--- src/app/mocktrading/page.tsx
+++ src/app/mocktrading/page.tsx
@@ -7,22 +7,20 @@ const ChartContainer = dynamic(() => import("./compoenents/ChartContainer"), {
 	ssr: false,
 });
 
-// const url = process.env.NEXT_PUBLIC_API_URL!!;
+const url = process.env.NEXT_PUBLIC_API_URL!!;
 // type candles = number[][];
 export default async function mocktraiding() {
 	const openTime = getRandomInt(100, 1239);
 	const fileNumber = getRandomInt(1, 64);
-	const candles = await fetch(
-		`https://long-short.vercel.app/api/data/${fileNumber}`
-	)
+	const candles = await fetch(`${url}/api/data/${fileNumber}`)
 		.then((res) => res.json())
 		.then((data) => data.data);
 	// const candles = (await getCandle()) as candles;
 
 	return (
-		<section className="p-4 md:flex md:gap-3">
+		<section className="flex h-full p-4 md:flex md:gap-3">
 			<ChartContainer candles={candles} openTime={openTime} />
-			<div className="w-full">
+			<div className="flex flex-col w-full">
 				<div className="mb-4 mt-2">
 					<MyWallet />
 				</div>
