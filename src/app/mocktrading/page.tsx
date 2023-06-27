e11a750301089cfc7372e84030122f2c3cca6905
Latemarch <dolphin0331@naver.com>
2023-06-28 00:44:41 +0900

    without server data

--- src/app/mocktrading/page.tsx
+++ src/app/mocktrading/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import HistoryPannel from "./compoenents/HistoryPannel";
-import { getRandomInt } from "@/libs/client/utils";
+import { getCandle, getRandomInt } from "@/libs/client/utils";
 import ButtonContainer from "./compoenents/ButtonContainer";
 import MyWallet from "./compoenents/MyWallet";
 import { getCandles } from "@/libs/server/fetchingFtns";
@@ -9,15 +9,15 @@ const ChartContainer = dynamic(() => import("./compoenents/ChartContainer"), {
 });
 
 const url = process.env.NEXT_PUBLIC_API_URL!!;
-// type candles = number[][];
+type candles = number[][];
 export default async function mocktraiding() {
 	const openTime = getRandomInt(100, 1239);
 	const fileNumber = getRandomInt(1, 64);
 	// const candles = await fetch(`${url}/api/data/${fileNumber}`)
 	// 	.then((res) => res.json())
 	// 	.then((data) => data.data);
-	// const candles = (await getCandle()) as candles;
-	const candles = await getCandles(`${url}/api/data/${fileNumber}`);
+	const candles = (await getCandle()) as candles;
+	// const candles = await getCandles(`${url}/api/data/${fileNumber}`);
 
 	return (
 		<section className="h-full p-4 md:flex md:gap-3">
